874450fadf3cb9b1df72b96afa88726057017ede
ezntek <ezntek@xflymusic.com>
2023-04-02 13:56:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 import subprocess
 import shutil
 import os
-from pip._internal import main as pip_main
 
 def setup() -> None:
     while True: # start a loop
         ans: str = input("Use pacman or apt? ")
         if ans.lower() == "pacman":
-            # use su because not all use sudo, some use doas or other utilities
+            # use su
             r = subprocess.run('su -c "pacman -S python3\
                                 python3-pip\
                                 python3-tk\
@@ -40,7 +39,7 @@ def setup() -> None:
             print("Invalid Answer, try again!")
         # return to top
     
-    pip_main(['install', '-U', 'pysimplegui']) # run pip from within python
+    subprocess.run(["python3", "-m", "pip", "install", "pysimplegui"])
     shutil.copytree("./src", f"{os.environ['HOME']}/SM64LinuxLauncher") # copy into home folder
 
 if __name__ == "__main__":
